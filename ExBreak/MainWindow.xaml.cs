fd5a1cd15e356b8629cf8a88a0c917880602f0e9
Da-Cheng-Zi <54072867+Da-Cheng-Zi@users.noreply.github.com>
2021-06-26 19:16:50 +0800

    注释

---
+++ ExBreak/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExBreak
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            string str = Result.Text;
+            string str1 = str.Substring(0, 32);
+            string[] sArray = str.Split('x');
+            string str2 = sArray[0].Remove(0,32);
+
+
+            HashID.Text = str1;
+            Num.Text = str2;
+            Igneous.Text = sArray[1];
+            
+        }
+    }
+}
